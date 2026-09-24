c8223ff122791d5b3d7aa53511bf4fce53576be8
Rishabh Mishra <rishabhmishra872660@gmail.com>
2026-09-24 11:11:17 +0530

    Time: 10 ms (45.64%), Space: 7.6 MB (88.26%) - CodeHub

---
+++ LeetCode/0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        // Base case
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+
+        // Recursive case
+        int ans = fib(n-1) + fib(n-2);
+        return ans;
+    }
+};
